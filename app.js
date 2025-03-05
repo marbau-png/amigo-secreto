34c4d704417f374d8561b54b8ffa0cea48d353c6
marbau-png <marbau2049@gmail.com>
2025-03-05 19:56:59 -0300

    Modificación de archivo app

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [ "Fernanda", "Damian", "Agustina", "Carolina", "Javier", "Laura", "Sofía", "Marcos", "Natalia", "Daniel" ];
+
+console.log (amigos.length);
+//10
