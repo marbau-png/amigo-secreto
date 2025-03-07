f7f64fc5acebb9ff4d38aeea28df1ba969e5ca78
marbau-png <marbau2049@gmail.com>
2025-03-07 18:40:54 -0300

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// almacenar los nombres de los amigos ingresados
+const amigos = [];
 
-let amigos = [ "Fernanda", "Damian", "Agustina", "Carolina", "Javier", "Laura", "Sofía", "Marcos", "Natalia", "Daniel" ];
+// capturar el valor del campo de entrada
+function agregarAmigos () {
+    const input = document.querySelector ("amigo")
+    const nombre = input.value.trim ();
 
-console.log (amigos.length);
-//10
+
+// validar la entrada
+if (nombre === "") {
+    alert ("Por favor, ingrese un nombre");
+}
+
+// actualizar y limpiar el campo de entrada
+amigos.push (nombre);
+actualizarLista();
+input.value = ""; 
+
+}
+
+
